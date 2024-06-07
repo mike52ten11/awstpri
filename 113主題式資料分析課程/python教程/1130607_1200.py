5890465fe3dfdbd7e0873aeec619191d296d0f05
mike52ten11 <mike3838038539@gmail.com>
2024-06-07 12:12:08 +0800

    first commit

---
+++ 113主題式資料分析課程/python教程/1130607_1200.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 一、賦值
+
+# ## 1.文字
+
+# In[4]:
+
+
+a='hello world'
+print(a)
+
+
+# ## 2.數字
+
+# In[ ]:
+
+
+a=5
+print(a)
+
+
+# ## 3.陣列
+
+# ### 3.1 字典
+
+# In[10]:
+
+
+a={'date':'2024-01-01 04:00',
+       'name':'tpri',
+      'age':18}
+print(a)
+
+
+# # 二、迴圈
+
+# In[ ]:
+
+
+for i in range(10):
+    print(i)
+
+
+# In[ ]:
+
+
+
+
