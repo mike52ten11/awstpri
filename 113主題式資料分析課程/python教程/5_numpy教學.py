528900154455d91def9a52d1401adfb38f8961ee
mike52ten11 <mike3838038539@gmail.com>
2024-06-07 15:02:40 +0800

    latest version

--- 113主題式資料分析課程/python教程/5_numpy教學.py
+++ 113主題式資料分析課程/python教程/5_numpy教學.py
@@ -70,6 +70,61 @@ a.transpose((1,0))#a 列轉置
 a.transpose((0,1))#a 行轉置
 
 
+# ## 內積
+
+# In[34]:
+
+
+inner_product  = np.dot(np.array([1,2,3]) ,np.array([4,5,6]))
+print('用np.dot = ',inner_product)
+
+inner_product  = np.array([1,2,3]) @ np.array([4,5,6])
+print('用@ = ',inner_product)
+
+
+
+# ## 逐元素乘法
+
+# In[40]:
+
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+elementwise_product = np.zeros((3),dtype=int)
+for i in range(3):
+    elementwise_product[i] = a[i]*b[i]
+
+print('用for 迴圈:',elementwise_product)
+
+#想要每個元素相乘可以使用*就好了
+elementwise_product = np.array([1,2,3]) * np.array([4,5,6])
+print('用np函式',elementwise_product)
+
+
+# ## cos 相似性
+
+# In[23]:
+
+
+import numpy as np
+
+# 定義兩個 numpy array
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# 計算內積
+dot_product = np.dot(a, b)
+
+# 計算 L2 範數
+norm_a = np.linalg.norm(a)
+norm_b = np.linalg.norm(b)
+
+# 計算余弦相似性
+cosine_similarity = dot_product / (norm_a * norm_b)
+
+print("余弦相似性:", cosine_similarity)
+
+
 # In[ ]:
 
 
